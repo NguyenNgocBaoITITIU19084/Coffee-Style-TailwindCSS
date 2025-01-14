142db15128ff16128e7fa4b4b1fd40626d514c60
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2025-01-14 12:06:59 +0700

    add story

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       colors: {
         "light-coffee": "#C89F94",
+        coffee: {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F48",
+          600: "#744838",
+        },
       },
       keyframes: {
         slideDown: {
